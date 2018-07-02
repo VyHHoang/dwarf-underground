a7fdb043d62b60d6accad4ed8eac9b346f1a6276
Vy H Hoang <vyhoang@vys-macbook-pro.coldspring.marian.edu>
2018-07-02 19:23:35 -0400

    Makingn components

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react' 
+
+class Footer extends Component {
+    render() {
+      return (
+        <Footer className="expanded row">
+          <h6>&copy; 2017 Erebor Industries</h6>
+          <h6 className="float-right">All rights reserved</h6>
+          </Footer>
+    
+      )
+      }
+    }
+export default Footer
